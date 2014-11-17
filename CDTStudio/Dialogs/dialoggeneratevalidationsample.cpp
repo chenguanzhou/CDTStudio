84e06d803aee556c08795feac7a4740b6211c625
cgz <chenguanzhou123@163.com>
2014-11-18 00:28:59 +0800

    add CDTBlackListValidator

--- CDTStudio/Dialogs/dialoggeneratevalidationsample.cpp
+++ CDTStudio/Dialogs/dialoggeneratevalidationsample.cpp
@@ -1,14 +1,40 @@
 #include "dialoggeneratevalidationsample.h"
 #include "ui_dialoggeneratevalidationsample.h"
 
-DialogGenerateValidationSample::DialogGenerateValidationSample(QWidget *parent) :
+#include "stable.h"
+
+DialogGenerateValidationSample::DialogGenerateValidationSample(QString projectID, QWidget *parent) :
     QDialog(parent),
-    ui(new Ui::DialogGenerateValidationSample)
+    ui(new Ui::DialogGenerateValidationSample),
+    model(new QSqlQueryModel(this)),
+    prjID(projectID)
 {
-    ui->setupUi(this);
+    ui->setupUi(this);    
+    ui->comboBoxPointset->setModel(model);
+    updateCombobox();
+    QStringList validationNames = getValidationNames();
+    CDTBlackListValidator *validator = new CDTBlackListValidator(validationNames,this);
+    ui->lineEditName->setValidator(validator);
 }
 
 DialogGenerateValidationSample::~DialogGenerateValidationSample()
 {
     delete ui;
+}
+
+void DialogGenerateValidationSample::updateCombobox()
+{
+    model->setQuery(QString("select pointset_name from points_project where projectid = '%1'")
+                    .arg(prjID),QSqlDatabase::database("category"));
+}
+
+QStringList DialogGenerateValidationSample::getValidationNames() const
+{
+    QSqlQuery query(QSqlDatabase::database("category"));
+    query.exec(QString("select name from image_validation_samples"));
+    QStringList list;
+    while (query.next()) {
+        list<<query.value(0).toString();
+    }
+    return list;
 }
--- CDTStudio/Dialogs/dialoggeneratevalidationsample.h
+++ CDTStudio/Dialogs/dialoggeneratevalidationsample.h
@@ -2,21 +2,47 @@
 #define DIALOGGENERATEVALIDATIONSAMPLE_H
 
 #include <QDialog>
+#include <QValidator>
 
 namespace Ui {
 class DialogGenerateValidationSample;
 }
+class QSqlQueryModel;
 
 class DialogGenerateValidationSample : public QDialog
 {
     Q_OBJECT
 
 public:
-    explicit DialogGenerateValidationSample(QWidget *parent = 0);
-    ~DialogGenerateValidationSample();
 
 private:
+    explicit DialogGenerateValidationSample(QString projectID,QWidget *parent = 0);
+    ~DialogGenerateValidationSample();
+
+    void updateCombobox();
+    QStringList getValidationNames() const;
+
     Ui::DialogGenerateValidationSample *ui;
+    QSqlQueryModel *model;
+    QString prjID;
+};
+
+class CDTBlackListValidator:public QValidator
+{
+    Q_OBJECT
+public:
+    explicit CDTBlackListValidator(QStringList blackList, QObject * parent = 0)
+        :QValidator(parent),blackLst(blackList){}
+
+    State validate(QString &input, int &pos) const
+    {
+        if (blackLst.contains(input))
+            return Invalid;
+        else
+            return Acceptable;
+    }
+private:
+    QStringList blackLst;
 };
 
 #endif // DIALOGGENERATEVALIDATIONSAMPLE_H
