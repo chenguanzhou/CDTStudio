964ac67536e52574352e08d732b712ec11d7a1ca
cgz <chenguanzhou123@163.com>
2016-07-25 17:13:30 +0800

    read the docs gitignore

--- docs/conf.py
+++ docs/conf.py
@@ -39,11 +39,17 @@ extensions = [
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
+from recommonmark.parser import CommonMarkParser
+
+source_parsers = {
+    '.md': CommonMarkParser,
+}
+
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-# source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
+# source_suffix = '.rst'
 
 # The encoding of source files.
 #
